539a47509569fe809bf7e7f3f04de81a23e88a54
Mawanda <giovanni.cavallin@outlook.com>
2018-05-31 15:05:05 +0200

    Modificati e aggiunti test

--- new marvin/scripts/test.js
+++ new marvin/scripts/test.js
@@ -1,21 +1,43 @@
-//useful for mounting pages
-require('jsdom-global/register')
-
-// const babel_register = require('babel-register')
-
 // setting the url for the environment: to change at build time
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
+//useful for mounting pages
+var babel = require('babel-register')()
+
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+// global.document = jsdom
+global.navigator = { userAgent: 'node.js' }
+global.window = document.defaultView
+
+var exposedProperties = ['window', 'navigator', 'document']
+
+Object.keys(document.defaultView)
+    .forEach((property) => {
+        if (typeof global[property] === 'undefined') {
+            exposedProperties.push(property)
+            global[property] = document.defaultView[property]
+        }
+    })
+
+// const babel_register = require('babel-register')
+require.extensions['.css'] = function () { return null }
+require.extensions['.jpg'] = function () { return null }
+
 // requiring Mocha files and other tools to iterate into the folder
 var Mocha = require('mocha'),
-    fs = require('fs-extra'),
-    path = require('path');
+    fs = require('fs-extra');
+// path = require('path');
 
 var mocha = new Mocha({
-    reporter: 'nyan', //put 'nyan' and see
+    reporter: 'spec', //put 'nyan' and see
     useColors: true,
-    require: require('babel-register')()
+    require: babel
 });
 
 //this is the folder in which we have to find the test files
@@ -25,69 +47,34 @@ var testDir = 'src';
 function lookingForTests(dir) {
     return new Promise(function (resolve, reject) {
         walk(dir, function (err, data) {
-            if(err !== null) return reject(err);
+            if (err !== null) return reject(err);
             resolve(data);
         })
     })
 }
 
-// function to recursively and parallely look for spec.js files into the 'dir' folder
-// var walk = function (dir) {
-//     var results = [];
-//     fs.readdir(dir, function (err, list) {
-//         if(err) return
-//         var pending = list.length;
-//         if(!pending) return true
-//         list
-//             .forEach(function (file) {
-//                 if(file.substr(-8) === '.test.js') {
-//                     console.log(file) //it will list all tests files
-//                     mocha.addFile(
-//                         path.join(dir, file)
-//                     );
-//                 }
-//                 file = path.join(dir, file);
-//                 fs.stat(file, function (err, stat) {
-//                     if(stat && stat.isDirectory() && stat === 'node_modules') return
-//                     if(stat && stat.isDirectory()) {
-//                         walk(file, function (err, res) {
-//                             results = results.concat(res);
-//                             if(!--pending) return
-//                         });
-//                     } else {
-//                         results.push(file);
-//                         if(!--pending) return;
-//                     }
-//                 });
-//             });
-//     });
-// };
-
 // function to recursively and sequentially look for test.js files into the 'dir' folder
 var walk = function (dir, done) {
     var results = [];
     fs.readdir(dir, function (err, list) {
-        if(err) return done(err);
+        if (err) return done(err);
         var i = 0;
         (function next() {
             var file = list[i++];
-            if(!file) return done(null, results);
+            if (!file) return done(null, results);
             file = dir + '/' + file;
             fs.stat(file, function (err, stat) {
-                if(stat && stat.isDirectory() && stat === 'node_modules') next()
+                if (stat && stat.isDirectory() && stat === 'node_modules') next()
                 else {
-                    if(stat && stat.isDirectory()) {
+                    if (stat && stat.isDirectory()) {
                         walk(file, function (err, res) {
                             results = results.concat(res);
                             next();
                         });
                     } else {
-                        // results.push(file);
-                        if(file.substr(-8) === '.test.js') {
+                        if (file.substr(-8) === '.test.js') {
                             console.log(file) //it will list all tests files
-                            mocha.addFile(file
-                                //path.join(dir, file)
-                            );
+                            mocha.addFile(file);
                         }
                         next();
                     }
@@ -103,5 +90,3 @@ lookingForTests(testDir)
         process.exitCode = failures ? -1 : 0; // exit with non-zero status if there were failures
     }))
     .catch(err => console.log(err))
-//mocha.addFile('./src/components/__tests__/LoginButton.test.js')
-// // Run the tests.
