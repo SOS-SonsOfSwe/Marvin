70fff56e29fb4fabd08ce34498948339fa40d5de
Lmenegon <lorenzomenegon95@gmail.com>
2018-05-31 16:43:58 +0200

    Aggiunto test ExamList

---
+++ new marvin/src/components/__tests__/ExamList.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {mount} from 'enzyme'
+import chai from 'chai'
+import sinon from 'sinon';
+import ExamList from '../ExamList/ExamList';
+
+
+describe('<ExamList/>', () => {
+    it('renders without exploding', () => {
+        const wrapper = mount(<ExamList />);
+        chai.expect(wrapper.find(ExamList).length).to.equal(1);
+    });
+ });
