03574ac8e5d6c372a5f0eb28dccce18989b7b3f4
Unknown <federicocaldart@yahoo.it>
2018-06-13 19:27:19 +0200

    Fix autocompletamento

--- new marvin/src/containers/Profile/Admin/Courses/InsertCourseContainer.js
+++ new marvin/src/containers/Profile/Admin/Courses/InsertCourseContainer.js
@@ -3,21 +3,14 @@ import InsertCourse from '../../../../components/Profile/Admin/Courses/InsertCou
 import addNewCourse from '../../../../redux/actions/Admin/AddCourse'
 
 const mapStateToProps = (state, ownProps) => {
-
-  if(ownProps.location.state.fromDegree === false) {
-    return {
-      adding: state.admin.adding,
-      success: state.admin.success
-    }
-  } else {
-    return {
-      adding: state.admin.adding,
-      success: state.admin.success,
-      year: ownProps.location.state.year,
-      degreeUnicode: ownProps.location.state.degreeUnicode
-    }
+  return {
+    adding: state.admin.adding,
+    success: state.admin.success,
+    year: ownProps.location.state.year,
+    degreeUnicode: ownProps.location.state.degreeUnicode
   }
 }
+
 
 const mapDispatchToProps = {
   addCourse: addNewCourse
