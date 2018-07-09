8e2b151915dc330eb650348222525a5ade379d0f
Mawanda <giovanni.cavallin@outlook.com>
2018-07-09 19:06:30 +0200

    implementato hot swap: ora quando si cambia utente il logout e' automatico.

--- Marvin/api/utils/getWeb3.js
+++ Marvin/api/utils/getWeb3.js
@@ -2,7 +2,10 @@
 import store from '../../src/store'
 import Web3 from 'web3'
 
+import { logoutUser } from "../../src/redux/actions/Login-logout-signup/LogoutButtonActions"
+
 export const WEB3_INITIALIZED = 'WEB3_INITIALIZED'
+
 function web3Initialized(results) {
   return {
     type: WEB3_INITIALIZED,
@@ -10,14 +13,14 @@ function web3Initialized(results) {
   }
 }
 
-let getWeb3 = new Promise(function(resolve, reject) {
+let getWeb3 = new Promise(function (resolve, reject) {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
-  window.addEventListener('load', function(dispatch) {
+  window.addEventListener('load', function (dispatch) {
     var results
     var web3 = window.web3
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
+    if(typeof web3 !== 'undefined') {
       // Use Mist/MetaMask's provider.
       web3 = new Web3(web3.currentProvider)
 
@@ -28,6 +31,16 @@ let getWeb3 = new Promise(function(resolve, reject) {
       console.log('Injected web3 detected.');
 
       resolve(store.dispatch(web3Initialized(results)))
+
+      var account = web3.eth.accounts[0];
+      setInterval(function () {
+        if(web3.eth.accounts[0] !== account) {
+          account = web3.eth.accounts[0];
+          resolve(store.dispatch(logoutUser()));
+        }
+      }, 100);
+      // resolve(eraseReducers())
+      // resolve(store.dispatch(userLoggedOut()))
     } else {
 
       // Fallback to localhost if no web3 injection. We've configured this to
@@ -43,6 +56,8 @@ let getWeb3 = new Promise(function(resolve, reject) {
       console.log('No web3 instance injected, using Local web3.');
 
       resolve(store.dispatch(web3Initialized(results)))
+      // resolve(eraseReducers())
+      // resolve(store.dispatch(userLoggedOut()))
     }
   })
 })
