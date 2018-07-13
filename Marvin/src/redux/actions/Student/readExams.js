5440b9a6296eb54d113a7912c94d5cde1feba566
Mawanda <giovanni.cavallin@outlook.com>
2018-07-13 18:19:33 +0200

    Conversione parziale a await/async per readExams di student

--- Marvin/src/redux/actions/Student/readExams.js
+++ Marvin/src/redux/actions/Student/readExams.js
@@ -31,79 +31,101 @@ function doAwesomeStuff(dispatch, load) {
   // return browserHistory.push('/profile/degrees') //|| alert(payload.FC + " successfully logged in as " + utils.userDef(payload.tp) + " with badge number: " + payload.badgeNumber)
 }
 
-async function processIPFSResultParallel(ipfs, payload) {
-  const promises = payload.map(item => ipfs.getJSON(item.load)
-    .then(result => {
-      // here I overwrite the description information with the JSON returning from the ipfs
-      item.load = result
-    }))
+async function processIPFSResultParallel(payload) {
+  var ipfs = new ipfsPromise()
+  const promises = payload.map(async item =>
+    item.load = await ipfs.getJSON(item.load)
+    // here I overwrite the description information with the JSON returning from the ipfs
+  )
   await Promise.all(promises)
 }
 
-async function readExams(classInstance, classes, payload, web3, coinbase, dispatch) {
+async function readExams(classInstance, classes, web3, coinbase, dispatch) {
+  var payload
   return new Promise(function (resolve, reject) { // for(let sclass of classes)
-    classes.map(sclass => {
+    classes.map(async sclass => {
       // var payloadToReturn
       var classUnicode = web3.toUtf8(sclass)
-      return classInstance.getClassExamsData(classUnicode, { from: coinbase })
-        .then(result => {
+      try {
+        var result = await classInstance.getClassExamsData(classUnicode, { from: coinbase })
 
-          // result[0] = examHashcode
-          // result[1] = examsTeacher
-          // result[2] = examUnicode
+        // result[0] = examHashcode
+        // result[1] = examsTeacher
+        // result[2] = examUnicode
 
-          console.log('EXAMS READ RESULT: ')
-          console.log(result)
+        console.log('EXAMS READ RESULT: ')
+        console.log(result)
 
-          if(result[0].length === 0) {
-            return payload
-          } else {
+        if(result[0].length === 0) {
+          return payload
+        } else {
 
-            var hashIPFS
-            for(let j = 0; j < result[0].length; j++) {
-              var exam = result[2][j]
-              var hash = result[0][j]
-              var teac = web3.toDecimal(result[1][j])
-              console.log("teacher: " + teac)
-              var exUni = web3.toUtf8(exam)
-              // console.log('dgr: ' + dgr)
-              hashIPFS = ipfsPromise.getIpfsHashFromBytes32(hash)
-              console.log("hash: " + hashIPFS)
-              // i'm storing the informations inside the description. We will retrieve them later.
-              // console.error(payload == null)
-              if(payload == null) { // first element of array
-                payload = [{ load: hashIPFS, examUnicode: exUni, teacher: teac }, ]
-              } else
-                payload = [...payload,
-                  { load: hashIPFS, examUnicode: exUni, teacher: teac }
-                ]
-            }
-            var ipfs = new ipfsPromise()
-            return processIPFSResultParallel(ipfs, payload)
-              .then(() => {
-                payload.sort((a, b) => b.load.date - a.load.date)
-                // console.error('payload: ' + JSON.stringify(payload))
-                return resolve(payload)
-              })
-              .catch(function (error) {
-                // If error, go to signup page.
-                console.error('Error while reading ipfs informations.')
-                console.log(error)
-                dispatch(errorReadingData(req))
-                return reject(error)
-                // return browserHistory.push('/profile')
-              })
+          var hashIPFS
+          for(let j = 0; j < result[0].length; j++) {
+            var exam = result[2][j]
+            var hash = result[0][j]
+            var teac = web3.toDecimal(result[1])
+            console.log("teacher: " + teac)
+            var exUni = web3.toUtf8(exam)
+            // console.log('dgr: ' + dgr)
+            hashIPFS = ipfsPromise.getIpfsHashFromBytes32(hash)
+            console.log("hash: " + hashIPFS)
+            // i'm storing the informations inside the description. We will retrieve them later.
+            // console.error(payload == null)
+            if(payload == null) { // first element of array
+              payload = [{ load: hashIPFS, examUnicode: exUni, classUnicode: classUnicode, teacher: teac }, ]
+            } else
+              payload = [...payload,
+                { load: hashIPFS, examUnicode: exUni, classUnicode: classUnicode, teacher: teac }
+              ]
           }
+          try {
+            await processIPFSResultParallel(payload)
+            payload.sort((a, b) => b.load.date - a.load.date)
+            // console.error('payload: ' + JSON.stringify(payload))
+            return resolve(payload)
+          } catch(error) {
+            console.error('Error while reading ipfs informations.')
+            console.log(error)
+            dispatch(errorReadingData(req))
+            return reject(error)
+          }
+        }
+      } catch(error) {
+        console.error('Error while reading exams.')
+        console.log(error)
+        dispatch(errorReadingData(req))
+        return reject(error)
+      }
 
-        })
-        .catch(function (error) {
-          // If error, go to signup page.
-          console.error('Error while reading exams.')
-          console.log(error)
-          dispatch(errorReadingData(req))
-          return reject(error)
-          // return browserHistory.push('/profile')
-        })
+      // return classInstance.getClassExamsData(classUnicode, { from: coinbase })
+      //   .then(async result => {
+
+      //       // return processIPFSResultParallel(payload)
+      //       //   .then(() => {
+      //       //     payload.sort((a, b) => b.load.date - a.load.date)
+      //       //     // console.error('payload: ' + JSON.stringify(payload))
+      //       //     return resolve(payload)
+      //       //   })
+      //       //   .catch(function (error) {
+      //       //     // If error, go to signup page.
+      //       //     console.error('Error while reading ipfs informations.')
+      //       //     console.log(error)
+      //       //     dispatch(errorReadingData(req))
+      //       //     return reject(error)
+      //       //     // return browserHistory.push('/profile')
+      //       //   })
+      //     }
+
+      // })
+      // .catch(function (error) {
+      //   // If error, go to signup page.
+      //   console.error('Error while reading exams.')
+      //   console.log(error)
+      //   dispatch(errorReadingData(req))
+      //   return reject(error)
+      //   // return browserHistory.push('/profile')
+      // })
     })
   })
 }
@@ -145,24 +167,32 @@ export function readStudentExamsFromDatabase(badgeNumber) {
                     return degreeInstance.getClasses(degree, { from: coinbase })
                       .then(classes => {
                         sClass.deployed()
-                          .then(function (classInstance) {
-                            var payload
+                          .then(async function (classInstance) {
+                            try {
+                              var payload = await readExams(classInstance, classes, web3, coinbase, dispatch)
+                              if(payload == null) dispatch(dataEmpty(req))
+                              else return doAwesomeStuff(dispatch, payload)
+                            } catch(error) {
+                              console.error('Error in readExams.')
+                              console.log(error)
+                              dispatch(errorReadingData(req))
+                            }
                             // console.error('badgeNumber: ' + badgeNumber)
-                            return readExams(classInstance, classes, payload, web3, coinbase, dispatch)
-                              .then((payload) => {
-                                // console.error('payload: ' + JSON.stringify(payload))
-                                // console.error('i: ' + i)
-                                // here I dispatch all the exams the student is registered to
-                                if(payload == null) dispatch(dataEmpty(req))
-                                else return doAwesomeStuff(dispatch, payload)
-                              })
-                              .catch(function (error) {
-                                // If error, go to signup page.
-                                console.error('Error in readExams.')
-                                console.log(error)
-                                dispatch(errorReadingData(req))
-                                // return browserHistory.push('/profile')
-                              })
+                            // return readExams(classInstance, classes, web3, coinbase, dispatch)
+                            //   .then((payload) => {
+                            //     // console.error('payload: ' + JSON.stringify(payload))
+                            //     // console.error('i: ' + i)
+                            //     // here I dispatch all the exams the student is registered to
+                            //     if(payload == null) dispatch(dataEmpty(req))
+                            //     else return doAwesomeStuff(dispatch, payload)
+                            //   })
+                            //   .catch(function (error) {
+                            //     // If error, go to signup page.
+                            //     console.error('Error in readExams.')
+                            //     console.log(error)
+                            //     dispatch(errorReadingData(req))
+                            //     // return browserHistory.push('/profile')
+                            //   })
 
                           })
                           .catch(function (error) {
