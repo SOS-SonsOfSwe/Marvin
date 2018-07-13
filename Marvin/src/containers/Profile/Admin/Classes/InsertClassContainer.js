6aac0160b86f383d7f04efb4015c82290c5600c1
Fresho <federicocaldart@yahoo.it>
2018-07-13 13:02:39 +0200

    Aggiunto dropdown in inserimento classe

--- Marvin/src/containers/Profile/Admin/Classes/InsertClassContainer.js
+++ Marvin/src/containers/Profile/Admin/Classes/InsertClassContainer.js
@@ -1,20 +1,40 @@
 import { connect } from 'react-redux'
 import InsertClass from '../../../../components/Profile/Admin/Classes/InsertClass'
 import addNewClass from '../../../../redux/actions/Admin/AddClass'
+import { bindActionCreators } from 'redux'
+
+import { readDegreesFromDatabase } from '../../../../redux/actions/Admin/readDegrees'
+import { readAcademicYearsFromDatabase } from '../../../../redux/actions/Admin/readAcademicYears'
+
 
 const mapStateToProps = (state, ownProps) => {
   return {
     adding: state.admin.adding,
     success: state.admin.success,
     year: ownProps.location.state.year,
-    degreeUnicode: ownProps.location.state.degreeUnicode
+    degreeUnicode: ownProps.location.state.degreeUnicode,
+
+    degrees: state.admin.degrees.payload,
+    successDegree: state.admin.degrees.success,
+    academicYears: state.admin.academicYears.payload,
+    loadingDegree: state.admin.degrees.loading,
+    loadingAcademic: state.admin.academicYears.loading,
+    emptyClasses: state.admin.classes.empty,
+    emptyDegrees: state.admin.degrees.empty,
+    emptyAcademicYears: state.admin.academicYears.empty,
+
   }
 }
 
 
-const mapDispatchToProps = {
-  addClass: addNewClass
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addClass: addNewClass,
+    readDegreeData: bindActionCreators(readDegreesFromDatabase, dispatch),
+    readAcademicData: bindActionCreators(readAcademicYearsFromDatabase, dispatch)
+  }
 }
+
 
 const InsertClassContainer = connect(
   mapStateToProps,
