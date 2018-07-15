79f3ebeecdc2743b80b88c7bbba8c82f4add97ac
Mawanda <giovanni.cavallin@outlook.com>
2018-07-16 00:49:32 +0200

    rirpova

--- Marvin/src/utils/costs.js
+++ Marvin/src/utils/costs.js
@@ -1,10 +1,8 @@
 import ethPrice from 'eth-price'
 
-export async function getEurFromEth(ether) {
-  ethPrice('EUR')
-    .then(ethInEur => {
-      ethInEur = parseFloat(ethInEur[0]
-        .slice(5))
-      return(ethInEur * ether)
-    })
+export function getEurFromEth(ether) {
+  var ethInEur = ethPrice('EUR')
+  ethInEur = parseFloat(ethInEur[0]
+    .slice(5))
+  return(ethInEur * ether)
 }
