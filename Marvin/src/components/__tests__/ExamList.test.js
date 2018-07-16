446805290df756d9a8534ffd8a068b5ad9b44e62
Lmenegon <lorenzomenegon95@gmail.com>
2018-07-16 18:40:36 +0200

    aggiunto test di prova per examlist

--- Marvin/src/components/__tests__/ExamList.test.js
+++ Marvin/src/components/__tests__/ExamList.test.js
@@ -11,3 +11,38 @@ describe('<ExamList/>', () => {
         chai.expect(wrapper.find(ExamList).length).to.equal(1);
     });
 });
+
+
+
+//test di prova...
+/*
+import ExamList from '../Profile/Commons/ExamList';
+import { shallow } from 'enzyme'
+import React from 'react';
+import { expect } from 'chai';
+
+describe('AcademicYearContainer', () => {
+    function setup(degree, Sclass, typology, date){
+        const mockStore = {
+          degree: degree,
+          Sclass: Sclass,
+          typology: typology,
+          date: date,
+        };
+    
+        return shallow(<AcademicYears {...mockStore}/>);
+    }
+
+    it("The page load the correct number of exams", () =>{
+        const wrapper = setup(
+          'informatica',
+          '123',
+          'scienze',
+          [{date: '2017-2018'}]
+        )
+        expect(wrapper.find('Row').length).equals(1);
+        expect(wrapper.find('LoadingData').length).equals(0);
+    })
+
+})
+*/
