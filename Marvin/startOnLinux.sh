aa96702731ef64a4db5587ba793fc750dfbdf0bc
dallariva93 <giannix93@gmail.com>
2018-07-18 23:31:10 +0200

    aggiunto script linux senza databasing

--- Marvin/startOnLinux.sh
+++ Marvin/startOnLinux.sh
@@ -1,4 +1,4 @@
 gnome-terminal -e ./startBlockchain.sh &
 sleep 15
-gnome-terminal -e ./databasing_LoadProject.sh && fg
+gnome-terminal -e ./loadProjectAFTERstartingBlockchain.sh && fg
 
---
+++ Marvin/startOnLinuxAndDatabasing.sh
@@ -0,0 +1,4 @@
+gnome-terminal -e ./startBlockchain.sh &
+sleep 15
+gnome-terminal -e ./databasing_LoadProject.sh && fg
+
