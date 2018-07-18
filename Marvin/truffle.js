5c54abc0ba0024023ed4d28b810f19c3eedb1131
spanozzo <stefano.panozzo.1@studenti.unipd.it>
2018-07-18 23:07:54 +0200

    Cambiato poca roba prima della pubblicazione

--- Marvin/truffle.js
+++ Marvin/truffle.js
@@ -1,23 +1,3 @@
-//Rinkeby deploy
-/*
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "glue bamboo mesh urge glimpse truly vault reunion engine uncover strong capable";
-
-module.exports = {
-  networks: {
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/LRu2EZiQXhsNHvp7SCtK")
-      },
-      network_id: 4
-    }   
-  }
-};
-*/
-// mnemonic address 10+ ETH Rinkeby: smooth noodle laptop april bitter steak spare elevator orbit pipe climb shine
-// mnemonic address 0... ETH Rinkeby: army despair twelve hand humor elder inject race test fancy swap disorder
-// mnemonic address 15+ ETH Rinkeby:glue bamboo mesh urge glimpse truly vault reunion engine uncover strong capable
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
